caadec174bda2989a0289a4c1e9c41df04e496bd
mohit <bhadanimohit@1997@gmail.com>
2023-08-02 16:34:57 +0530

    Adding Frontend part of IPL Dashboard

---
+++ src/frontend/src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+const elem = <div><h1>Mohit Bhadani</h1></div>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(elem);
